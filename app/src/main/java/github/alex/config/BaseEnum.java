11035ca9b0acf2235c2f3769369241bc30e6faea
AlexCheung(张文亮) <qwe17911@sina.com>
2016-06-29 15:16:31 +0800

    V1.0.1 Mvp模式，Retrofit + OkHttp + RxJava，继续完善代码

--- app/src/main/java/github/alex/config/BaseEnum.java
+++
@@ -1,9 +0,0 @@
-package github.alex.config;
-
-/**
- * Created by Alex on 2016/6/13.
- */
-public class BaseEnum {
-    public static final String httpRequest = "网络请求";
-    public static final String showToast = "网络请求";
-}
